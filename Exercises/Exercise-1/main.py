059608eca7ec9358f9c4e3707211d1805b2ec0f8
Quang Le <145892384+Quanglee369@users.noreply.github.com>
2026-04-13 11:38:32 +0000

    None

--- Exercises/Exercise-1/main.py
+++ Exercises/Exercise-1/main.py
@@ -1,4 +1,10 @@
 import requests
+import aiohttp
+import zipfile
+import os
+import asyncio
+
+os.makedirs('downloads', exist_ok=True)
 
 download_uris = [
     "https://divvy-tripdata.s3.amazonaws.com/Divvy_Trips_2018_Q4.zip",
@@ -10,11 +16,28 @@ download_uris = [
     "https://divvy-tripdata.s3.amazonaws.com/Divvy_Trips_2220_Q1.zip",
 ]
 
+async def download_file(url:str, session: aiohttp.ClientSession) -> None:
+    try:
+        file_name = url.split("/")[-1]
+        file_path = 'downloads' + "//" + file_name
+        async with session.get(url) as response:
+            if response.status == 200:
+                with open(file_path, 'wb') as f:
+                    async for chunk in response.content.iter_chunked(8192):
+                        f.write(chunk)
+            else:
+                print(f"Some thing wrong with the zip file {file_name}, detail: {response.status}")
+        print(f"Successfully download zip file {file_name}")
+    except Exception as e:
+        print(f"Failed to download zip file {file_name}, detail: {e}")
 
-def main():
-    # your code here
-    pass
+def extract_csv(file_list: list) -> None:
+
+async def main():
+    async with aiohttp.ClientSession() as session:
+        task = [download_file(url = uri, session = session) for uri in download_uris]
+        await asyncio.gather(*task)
 
 
 if __name__ == "__main__":
-    main()
+    asyncio.run(main())
